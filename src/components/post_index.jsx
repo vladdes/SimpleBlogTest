bc6d05efdf160c68adf16134470bc035fc50070e
vladimir strukelj <vladimir.strukelj@gmail.com>
2018-09-04 21:42:52 +0200

    posts fetched-shown

--- src/components/post_index.jsx
+++ src/components/post_index.jsx
@@ -2,19 +2,28 @@ import React, { Component } from 'react';
 // import PostRecuder from '../reducers/reducer_posts';
 import { connect } from 'react-redux';
 import { fetchPosts } from './../actions/';
+import _ from 'lodash';
 
-class PostsIndex extends Component
-{
+class PostsIndex extends Component{
     componentDidMount(){
         this.props.fetchPosts();
     }
+
+    renderPosts(){
+       return _.map(this.props.posts, post => {
+            return <li className="list-group-item" key={post.id}>
+                {post.title}
+            </li>;
+       }); 
+    }
     render(){
 
-        console.log(this.props.posts);
         return (
             <div>
-                Posts Index
-               
+               <h3>Posts</h3>
+               <ul>
+                   {this.renderPosts()}
+               </ul>               
             </div>
         );
     }

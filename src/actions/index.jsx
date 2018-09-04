834e892f4e6578242643e630ccc17f696943153f
vladimir strukelj <vladimir.strukelj@gmail.com>
2018-09-04 21:35:59 +0200

    posts fetched

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -2,9 +2,14 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=teststuff1234';
+
 export function fetchPosts(){
-    const request = axios.get();
+    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
     return {
-        type: FETCH_POSTS
+        type: FETCH_POSTS,
+        payload: request
     }
 }
--- src/components/post_index.jsx
+++ src/components/post_index.jsx
@@ -1,14 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+// import PostRecuder from '../reducers/reducer_posts';
+import { connect } from 'react-redux';
+import { fetchPosts } from './../actions/';
 
 class PostsIndex extends Component
 {
+    componentDidMount(){
+        this.props.fetchPosts();
+    }
     render(){
+
+        console.log(this.props.posts);
         return (
             <div>
                 Posts Index
+               
             </div>
         );
     }
 }
 
-export default PostsIndex;
+function mapStatetoProps(state){
+    return { posts: state.posts};
+}
+
+export default connect(mapStatetoProps, { fetchPosts })(PostsIndex);

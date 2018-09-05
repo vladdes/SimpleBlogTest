7c0c0a419a058815d962ae0bbb73b5da4d4a286d
vladimir strukelj <vladimir.strukelj@gmail.com>
2018-09-05 22:18:48 +0200

    postsNew

--- src/components/post_index.jsx
+++ src/components/post_index.jsx
@@ -1,36 +1,42 @@
 import React, { Component } from 'react';
 // import PostRecuder from '../reducers/reducer_posts';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from './../actions/';
 import _ from 'lodash';
 
-class PostsIndex extends Component{
-    componentDidMount(){
+class PostsIndex extends Component {
+    componentDidMount() {
         this.props.fetchPosts();
     }
 
-    renderPosts(){
-       return _.map(this.props.posts, post => {
+    renderPosts() {
+        return _.map(this.props.posts, post => {
             return <li className="list-group-item" key={post.id}>
                 {post.title}
             </li>;
-       }); 
+        });
     }
-    render(){
+    render() {
 
         return (
             <div>
-               <h3>Posts</h3>
-               <ul>
-                   {this.renderPosts()}
-               </ul>               
+                <div className="text-xs-right">
+                    <Link className="btn btn-primary" to="/posts/new">
+                        Add a post
+                    </Link>
+                </div>
+                <h3>Posts</h3>
+                <ul>
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
 }
 
-function mapStatetoProps(state){
-    return { posts: state.posts};
+function mapStatetoProps(state) {
+    return { posts: state.posts };
 }
 
 export default connect(mapStatetoProps, { fetchPosts })(PostsIndex);
--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/post_index';
+import PostsNew from './components/posts_new';
 
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
@@ -14,9 +15,10 @@ const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
-      <div>
-        <Route path="/" component={PostsIndex} />      
-      </div>
+      <Switch>
+        <Route path="/" exact component={PostsIndex} />
+        <Route path="/posts/new" component={PostsNew} />
+      </Switch>
     </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
